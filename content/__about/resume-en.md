3700352d17487b9edddd26a0c7da88bd2c0b9c4f
caferion <caferion@ex-em.com>
2021-01-22 02:55:32 +0900

    GA 추가

---
+++ content/__about/resume-en.md
@@ -0,0 +1,13 @@
+---
+title: 'about'
+date: 2020-9-11 16:21:13
+lang: 'en'
+---
+
+# 곽동근
+
+<div align="center">
+
+블로그 개설 중
+
+</div>
---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `caferion`,
+  description: `Blog posted about ...`,
+  author: `caferion`,
+  introduction: `Front-End Developer`,
+  siteUrl: `https://caferion.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `caferion`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+  },
+  icon: `content/assets/favicon.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'caferion/blog-comments', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'caferion',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: 'G-5P5P5D0PFJ', // Add your google analytics tranking ID
+}
